748babdaef37f7c7156224560b578477654ee419
Cheburator <sashadanilenko06@gmail.com>
2025-05-20 15:49:06 +0200

    training cycles management added to admin panel

--- GTTG/bot/admin.py
+++ GTTG/bot/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import MuscleGroup, Exercise
+from .models import MuscleGroup, Exercise, TrainingCycle, CycleDay
+
 
 @admin.register(MuscleGroup)
 class MuscleGroupAdmin(admin.ModelAdmin):
@@ -10,4 +11,10 @@ class MuscleGroupAdmin(admin.ModelAdmin):
 class ExerciseAdmin(admin.ModelAdmin):
     list_display = ['name', 'muscle_group']
     list_filter = ['muscle_group']
+    search_fields = ['name']
+
+
+@admin.register(TrainingCycle)
+class TrainingCycleAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
     search_fields = ['name']
